8db7b285680661e9c692b0f81f3851a7ece046e4
tfjiao <jiaotengfei1016@163.com>
2021-05-08 00:14:48 +0800

    上传文件，debugtalk.py更新

--- debugtalk.py
+++ debugtalk.py
@@ -1,31 +1,20 @@
 import requests
+import os, time
+from bs4 import BeautifulSoup
+from httprunner import __version__
 from libs.model_001 import *
 from libs.model_002 import *
 
-#
-# # 配置环境的sesion,token
-# huanjing = [1, 2, 3]
-# def gen_session():
-#     if huanjing == 1:
-#         return "token1"
-#     elif huanjing ==2:
-#         return "token2"
-#     elif huanjing == 3:
-#         return "token3"
+def get_execution(host):
+    # 从get登录的html中获取post登录的变量值，传递给登录
+    res = requests.get(url=f"https://{host}:8888/cas/login", verify=False)
+    execution = BeautifulSoup(res.text, "lxml").find(attrs={"name":"execution"})["value"]
+    return execution
 
-
-def get_cookie():
-    url = "https://mubu.com/api/login/submit"
-    headers = {"User-Agent": "HttpRunner"}
-    body = {"phone": "18710748230", "password": "123456", "remember": "true"}
-    res = requests.post(url, headers=headers, data=body)
-    # return res.cookies["SESSION"]
-    print("这里是token:", res.cookies["Jwt-Token"])
-    print("这里是session:", res.cookies["SESSION"])
-    print("这里是cookie:", res.cookies)
-    print("这里是headers中的cookies:", res.headers["Set-Cookie"])
+def get_httprunner_version():
+    return __version__
 
 
 if __name__ == '__main__':
-    get_cookie()
+    get_execution()
 
--- libs/model_001.py
+++ libs/model_001.py
@@ -1,15 +1,17 @@
 import uuid
-
-# 这里是根目录的标记
-from httprunner import __version__
+import os, time
 
 
-def get_httprunner_version():
-    return __version__
-
-def gen_memeberid():
-    return "5242742441223623"
+def get_chengdu20_mapname():
+    """获取某个目录下的所有文件的文件名，不包括后缀名。该测试路径下下只有文件，没有目录。
+    :return:
+    """
+    file = "//ip//目录//目录"
+    li = [n.split(".")[0] for i in os.walk(file) for n in i[2]]
+    print(li)
+    return li
 
 # 随机生成一串字符串
 def gen_nodeid():
     return str(uuid.uuid4())
+
